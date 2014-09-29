7c489117c7c0f48241d82c30366ee04d9218efc3
Daniel Pyrathon <pirosb3@gmail.com>
2014-09-30 00:34:17 +0200

    remove duplicate adds from constructors

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -177,12 +177,7 @@ class ModelBase(type):
             new_class.add_to_class(obj_name, obj)
 
         # All the fields of any type declared on this model
-        new_fields = chain(
-            new_class._meta.local_fields,
-            new_class._meta.local_many_to_many,
-            new_class._meta.virtual_fields
-        )
-        field_names = set(f.name for f in new_fields)
+        field_names = set(f.name for f in new_class._meta.get_fields(include_parents=False))
 
         # Basic setup for proxy models.
         if is_proxy:
@@ -272,17 +267,6 @@ class ModelBase(type):
             # unless they have redefined any of them.
             if is_proxy:
                 new_class.copy_managers(original_base._meta.concrete_managers)
-
-            # Inherit virtual fields (like GenericForeignKey) from the parent
-            # class
-            for field in base._meta.virtual_fields:
-                if base._meta.abstract and field.name in field_names:
-                    raise FieldError(
-                        'Local field %r in class %r clashes '
-                        'with field of similar name from '
-                        'abstract base class %r' % (field.name, name, base.__name__)
-                    )
-                new_class.add_to_class(field.name, copy.deepcopy(field))
 
         if abstract:
             # Abstract base models can't be instantiated and don't appear in

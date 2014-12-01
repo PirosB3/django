ba0a82686d0fd8ee4b3db1caa6515ada813a6343
Daniel Pyrathon <pirosb3@gmail.com>
2014-12-01 19:33:50 +0100

    expire reverse cache too

--- django/db/models/options.py
+++ django/db/models/options.py
@@ -281,7 +281,7 @@ class Options(object):
                 field.related_model._meta._expire_cache(forward=False)
             except AttributeError:
                 pass
-        self._expire_cache(reverse=False)
+        self._expire_cache()
 
     def setup_pk(self, field):
         if not self.pk and field.primary_key:
